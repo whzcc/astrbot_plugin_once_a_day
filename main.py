7e3a4285b71ddfa9d97870a03f4f40bc3f63d036
whzcc <74447737+whzcc@users.noreply.github.com>
2025-03-01 16:35:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     # 注册指令的装饰器。指令名为“运势”。注册成功后，发送 `/运势` 就会触发这个指令，并将用户名写入数据库`
-    @filter.command("aabcd")
+    @filter.command("/运势")
     async def once_a_day(self, event: AstrMessageEvent):
         '''用户输入这个指令时，他的名字会被写入数据库''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         sender_id = event.get_sender_id()
@@ -67,11 +67,10 @@ class MyPlugin(Star):
         
         # 若用户今日未使用该指令，则is_ok(sender_id)返回True，否则返回False
         is_ok = is_input_allowed(sender_id)
+        logger.info(is_ok)
         if is_ok:
             yield event.plain_result(f"Hello, {sender_id}, 你发了 {message_str}!") # 发送一条纯文本消息
         else:
             yield event.plain_result(f"No!, {sender_id}, 你发了 {message_str}!") # 发送一条纯文本消息
 
         logger.info(message_chain)
-        print(event)
-        print(event.get_message_str())
